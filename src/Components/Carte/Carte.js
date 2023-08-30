4b5ef0cbc2d8bc5528c4681ab2f953869d9de9cf
Adonat74 <donatgoninet.antoine@gmail.com>
2023-08-30 18:08:30 +0000

    Mise en place des bounds ainsi que de l'image de la carte du festival

--- src/Components/Carte/Carte.js
+++ src/Components/Carte/Carte.js
@@ -1,26 +1,29 @@
 import React from "react";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup, ImageOverlay} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import { LatLngBounds, Map, CRS} from "leaflet";
 import "./Carte.css"
 
 export default function Carte () {
 
 
+    /*[40.712216, -74.22655], [40.773941, -74.12544]*/
+
+
+
+    const bounds = new LatLngBounds([-75.31147, -177.09238], [75.22568, 172.61348])
 
 
 
     return(
         <div id="map">
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+            <MapContainer center={[10.712216, -0.22655]} zoom={2} scrollWheelZoom={true}>
+            <ImageOverlay
+                url="http://drupal10/sites/default/files/2023-08/Capture%20d%27%C3%A9cran%202023-08-30%20175241.png"
+                bounds={bounds}
+                zIndex={10}
+/>
+                
             </MapContainer>
             
 
