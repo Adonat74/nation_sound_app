23db9d2bfdd6f2b633c591479f9d2696c337fb9e
Adonat74 <donatgoninet.antoine@gmail.com>
2024-01-29 12:48:30 +0100

    modified the FAQ to fetch the data from drupal

--- src/Components/Info_FAQ/FAQ.js
+++ src/Components/Info_FAQ/FAQ.js
@@ -1,39 +1,33 @@
-import "./Info_FAQ.css"
+import React from "react";
+import "./Info_FAQ.css";
+
 
 export default function FAQ () {
+
+    const [FAQData, setFAQData] = React.useState([]);
+
+    
+    React.useEffect(() => {
+        fetch('http://localhost/drupal10/jsonapi/node/faq')
+            .then(res => res.json())
+            .then(data => setFAQData(data.data));
+    }, []);
+
+    console.log(FAQData);
+
+    const FAQ = FAQData.map(data => {
+        return (
+            <div className="question">
+                <h2>{data.attributes.title}</h2>
+                <p>{data.attributes.field_reponse}</p>
+            </div> 
+        );
+    });
 
     return(
         <div className="FAQ">
             <h1>FAQ</h1>
-            <div className="question">
-                <h2>Question 1 Lorem ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis, nibh sit amet malesuada venenatis, arcu urna fermentum justo, accumsan placerat quam justo non tellus. Suspendisse leo ligula, tincidunt ut efficitur a, rhoncus sit amet metus. Fusce feugiat libero nulla, vitae laoreet elit elementum elementum.</p>
-            </div>
-            <div className="question">
-                <h2>Question 2 Lorem ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis, nibh sit amet malesuada venenatis, arcu urna fermentum justo, accumsan placerat quam justo non tellus. Suspendisse leo ligula, tincidunt ut efficitur a, rhoncus sit amet metus. Fusce feugiat libero nulla, vitae laoreet elit elementum elementum.</p>
-            </div>
-            <div className="question">
-                <h2>Question 3 Lorem ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis, nibh sit amet malesuada venenatis, arcu urna fermentum justo, accumsan placerat quam justo non tellus. Suspendisse leo ligula, tincidunt ut efficitur a, rhoncus sit amet metus. Fusce feugiat libero nulla, vitae laoreet elit elementum elementum.</p>
-            </div>
-            <div className="question">
-                <h2>Question 4 Lorem ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis, nibh sit amet malesuada venenatis, arcu urna fermentum justo, accumsan placerat quam justo non tellus. Suspendisse leo ligula, tincidunt ut efficitur a, rhoncus sit amet metus. Fusce feugiat libero nulla, vitae laoreet elit elementum elementum.</p>
-            </div>
-            <div className="question">
-                <h2>Question 5 Lorem ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis, nibh sit amet malesuada venenatis, arcu urna fermentum justo, accumsan placerat quam justo non tellus. Suspendisse leo ligula, tincidunt ut efficitur a, rhoncus sit amet metus. Fusce feugiat libero nulla, vitae laoreet elit elementum elementum.</p>
-            </div>
-            <div className="question">
-                <h2>Question 6 Lorem ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis, nibh sit amet malesuada venenatis, arcu urna fermentum justo, accumsan placerat quam justo non tellus. Suspendisse leo ligula, tincidunt ut efficitur a, rhoncus sit amet metus. Fusce feugiat libero nulla, vitae laoreet elit elementum elementum.</p>
-            </div>
-            <div className="question">
-                <h2>Question 7 Lorem ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis, nibh sit amet malesuada venenatis, arcu urna fermentum justo, accumsan placerat quam justo non tellus. Suspendisse leo ligula, tincidunt ut efficitur a, rhoncus sit amet metus. Fusce feugiat libero nulla, vitae laoreet elit elementum elementum.</p>
-            </div>
-            
+            {FAQ}
         </div>
     );
 }
