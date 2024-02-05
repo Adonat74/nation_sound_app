5b46d91c00110c143c42e649877af6c9694ab287
Adonat74 <donatgoninet.antoine@gmail.com>
2024-02-05 11:03:50 +0100

    Added the price and a select for the number of tickets

--- src/Paypal/PayPal.js
+++ src/Paypal/PayPal.js
@@ -1,8 +1,12 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import './PayPal.css'
 
 export default function PayPal() {
   const paypal = useRef();
+
+  const [price, setPrice] = useState(50);
+
+
 
   useEffect(() => {
     window.paypal
@@ -34,7 +38,25 @@ export default function PayPal() {
 
   return (
     <div className="paypal">
+        <div className="numberTicketSelect">
+            <h1>Billeterie</h1>
+            <p>1 Billet = 50.00 EUR</p>
+            <div>
+                <select 
+                    id="numberTicket" 
+                    value={price}
+                    onChange={(e) => setPrice(parseInt(e.target.value))}
+                >
+                    <option value="50">1 billet</option>
+                    <option value="100">2 billets</option>
+                    <option value="150">3 billets</option>
+                    <option value="200">4 billets</option>
+                </select>
+            </div>
+            <p>Total : € {price}</p>
+        </div>
         <div className="paypalContainer">
+            <h2>Règlement :</h2>
             <div ref={paypal}></div>
         </div>
     </div>
